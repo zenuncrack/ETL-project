2ecf0ed972d090f93e6bc4dca7b9cdb4466fb119
Zenun Chowdhury <zenun786@gmail.com>
2026-04-09 19:37:27 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+# Changed from .post to .get
+@app.get("/")
+async def hello():
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
